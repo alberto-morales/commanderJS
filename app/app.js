95a82279c7e78923707b09d7649180325ed30979
Alberto Morales <mail@albertomorales.eu>
2017-02-11 16:05:30 +0000

    primer intento de ver los logs

--- app/app.js
+++ app/app.js
@@ -16,7 +16,7 @@
 		  "optionsSuccessStatus" : 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 		};
 	app.use(cors(corsOptions));
-		
+	
 	var router = express.Router();
 	router.get('/', function(req, res) { 
 		 res.send("Welcome to Commander API v.0.0.1");
@@ -28,6 +28,77 @@
 	app.use('/rest', apirouter);
 
 	// Start server
-	app.listen(appSettings.port, function() {		
+	var server = app.listen(appSettings.port, function() {		
 		 console.log('Node server running on http://localhost:'+appSettings.port);
 	});
+
+	var io  = require('socket.io')(server, { path: '/logViewer'});
+//	io.on('connection', function(socket) {
+//	    console.log('a user connected with id %s', socket.id);
+//	    socket.on('ackLogViewer', function(data) {
+//			console.log('echo receiving ' + data);
+//		    setTimeout(function(){ 
+//							var f=new Date();
+//							var cad=f.getHours()+":"+f.getMinutes()+":"+f.getSeconds(); 
+//							cad = 'Patrullando a las ' + cad;
+//							console.log('broadcasting '+cad);
+//							socket.emit('clv', cad);
+//						 }, 
+//					   2000
+//					  );			
+//		});
+//	    setTimeout(function(){ 
+//	    						var f=new Date();
+//	    						var cad=f.getHours()+":"+f.getMinutes()+":"+f.getSeconds(); 
+//	    						cad = 'Patrullando a las ' + cad;
+//	    						console.log('broadcasting '+cad);
+//	    						socket.emit('clv', cad);
+//	    					 }, 
+//	    		   2000
+//	    		  );
+//	});
+
+	var lastACK = new Date().getTime();
+	
+	function compruebaPerdidaConexion(socket, remoteFileHarvester) {
+		var actualTimestamp = new Date().getTime();
+		if (actualTimestamp - lastACK > 10000) {
+			socket.disconnect();
+			remoteFileHarvester.end();
+		} else {
+		   lastACK = actualTimestamp;
+			setTimeout(function() {
+				compruebaPerdidaConexion(socket, remoteFileHarvester);
+			}, 10000);	
+		}
+	}
+	
+	io.on('connection', function(socket) {
+	    console.log('a user connected with id %s', socket.id);
+	    
+//	    socket.on('ackLogViewer', function(data) {
+//	    	lastACK = new Date().getTime();
+//	    });
+	    
+	    socket.on('keepAliveLogViewer', function(data) {
+	    	console.log('keeping alive server');
+	    	lastACK = new Date().getTime();
+	    });
+	    
+		var remoteFileHarvester = require('./util/remoteFileHarvester');
+		remoteFileHarvester.sendData('/hcis/orawls12c/deployments/logs/hcis/RecDatos.log',
+									 {"host" : "10.196.18.67",
+									  "port" : 22,
+									  "username" : "weblogic",
+									  "password" : "eRUWtWVotEq6XDIvW+l5ew=="
+									 },
+									 function(data) {
+										 socket.emit('clv', ''+data)
+									 }
+									);
+		setTimeout(function() {
+			compruebaPerdidaConexion(socket, remoteFileHarvester);
+		}, 10000);	
+
+	});
+	
